8096293ea97c003e324a09363eb905f42b7605eb
t-uejo <tiki.ur.tiki@icloud.com>
2024-12-22 13:26:08 +0900

    logoとfaviconを切り替え

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,7 +64,7 @@ const config: Config = {
       title: 'Taiki Uejo',
       logo: {
         alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        src: 'img/logo.png',
       },
       items: [
         {
